c196ebe921e7332f1382f679ad30b024e87ceacd
Peter <pdev6373@gmail.com>
2024-06-04 20:23:04 +0100

    made resend otp endpoint dynamic

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,4 +1,4 @@
+export { rateLimiter } from './rateLimiter';
 export { default as logging } from './logging';
 export { verifyJWT } from './verifyJWT';
-export { rateLimiter } from './rateLimiter';
 export { validateData } from './validation';
