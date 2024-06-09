77669a62c3ea69ff17b74fa76320c52911006e05
Peter <pdev6373@gmail.com>
2024-06-09 23:24:53 +0100

    cleanup routes

--- src/middlewares/logging.ts
+++ src/middlewares/logging.ts
@@ -1,44 +1,35 @@
-// src/middlewares/logging.middleware.ts
 import Chalk from 'chalk';
 
 export default class Logging {
-  public static info = (args: any): void => {
-    // eslint-disable-next-line no-console
+  public static info = (args: any): void =>
     console.log(
       Chalk.blue(`
 [${new Date().toLocaleString()}]
 [Log]:`),
       typeof args === 'string' ? Chalk.blueBright(args) : args,
     );
-  };
 
-  public static warn = (args: any): void => {
-    // eslint-disable-next-line no-console
+  public static warn = (args: any): void =>
     console.log(
       Chalk.yellow(`
 [${new Date().toLocaleString()}]
 [Info]:`),
       typeof args === 'string' ? Chalk.yellowBright(args) : args,
     );
-  };
 
-  public static error = (args: any): void => {
-    // eslint-disable-next-line no-console
+  public static error = (args: any): void =>
     console.log(
       Chalk.red(`
 [${new Date().toLocaleString()}]
 [Warn]:`),
       typeof args === 'string' ? Chalk.redBright(args) : args,
     );
-  };
 
-  public static log = (args: any): void => {
-    // eslint-disable-next-line no-console
+  public static log = (args: any): void =>
     console.log(
       Chalk.green(`
 [${new Date().toLocaleString()}]
 [Error]:`),
       typeof args === 'string' ? Chalk.greenBright(args) : args,
     );
-  };
 }
